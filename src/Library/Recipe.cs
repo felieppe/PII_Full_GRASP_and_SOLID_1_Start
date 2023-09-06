deda24efaebd6a414060620ef6f8caeb59c2113a
felieppe <me@felieppe.com>
2023-09-06 12:16:17 -0300

    Added GetSteps() func

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -16,6 +16,9 @@ namespace Full_GRASP_And_SOLID.Library
 
         public Product FinalProduct { get; set; }
 
+        public ArrayList GetSteps() {
+            return this.steps;
+        }
         public void AddStep(Step step)
         {
             this.steps.Add(step);
